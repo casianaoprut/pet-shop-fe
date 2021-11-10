6a5eb918cc7d8e6ac87f4c92a7368b1fc3fc31f0
casianaoprut <oprunicoletacasiana@yahoo.com>
2021-11-10 12:38:11 +0200

    Fixed a logic mistake.

--- src/app/cart/cart.service.ts
+++ src/app/cart/cart.service.ts
@@ -14,11 +14,19 @@ export class CartService {
   public addProductToCart(product: Product, quantity: number): void{
     const cart: CartElement[] = localStorage.getItem(this.cartKey) != null ?
                                 JSON.parse(<string>localStorage.getItem(this.cartKey)) : [];
-    cart.push({
-      product: product,
-      quantity: quantity
-    });
+    this.updateElementInCart(cart, {product: product, quantity: quantity});
     localStorage.setItem(this.cartKey,JSON.stringify(cart));
+  }
+
+  private updateElementInCart(cart: CartElement[], cartElement: CartElement): CartElement[]{
+    const elemIndex = cart.findIndex((elem => {
+      console.log(elem.product.name);
+      console.log(cartElement.product.name);
+      return elem.product.name == cartElement.product.name
+    }));
+    elemIndex == -1? cart.push(cartElement) : cart[elemIndex] = {product: cartElement.product,
+                                                                 quantity: cartElement.quantity + cart[elemIndex].quantity};
+    return cart;
   }
 
   public getCartProducts(): CartElement[]{
